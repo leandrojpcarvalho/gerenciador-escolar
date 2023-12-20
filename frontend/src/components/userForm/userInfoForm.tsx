7143154d50c761786c2d85cfcc12322a27268a1b
Leandro <ljpcarvalho@gmail.com>
2023-12-20 16:58:22 -0300

    feat: form verify info user

--- frontend/src/components/userForm/userInfoForm.tsx
+++ frontend/src/components/userForm/userInfoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, HtmlHTMLAttributes, useState } from 'react';
 import './userInfoForm.css';
 
 const INIT_STATE = {
@@ -12,7 +12,7 @@ const INIT_STATE = {
 function UserInfoForm() {
   const [form, setForm] = useState(INIT_STATE);
 
-  const handleOnChange = (event: React.FormEvent<HTMLInputElement>) => {
+  const handleOnChange = (event: React.InputHTMLAttributes<HTMLElement>) => {
     const key = event.target.name;
     const value = event.target.value;
 
@@ -24,71 +24,78 @@ function UserInfoForm() {
   const { treatment, cpf, email, name, pronouns } = form;
   return (
     <div className="form">
-      <h2>Olá!</h2>
-      <p>
-        Identificamos que é o seu primeiro acesso, vamos confirmar algumas
-        informações, prometo que demorará menos que 3 min
-      </p>
-
-      <h2>Com relação aos seus dados pessoais...</h2>
+      <h3>informações pessoais</h3>
       <form>
-        <div>
-          <label htmlFor="treatment">título</label>
-          <select
-            name="treatment"
-            defaultValue={'default'}
-            onChange={handleOnChange}
-            value={treatment}>
-            <option
-              value="default"
-              disabled
+        <div className='info'>
+          <div className='line'>
+            <label htmlFor="treatment">título</label>
+            <select
+              name="treatment"
+              defaultValue={'default'}
+              onChange={handleOnChange}
+              value={treatment}>
+              <option
+                value="default"
+                disabled
+              >
+                selecione
+              </option>
+              <option value="sr">senhor</option>
+              <option value="sra">senhora</option>
+              <option value="nf">sem formalidade</option>
+            </select>
+          </div>
+          <div className='line'>
+            <label htmlFor="pronouns">pronomes</label>
+            <select
+              name="pronouns"
+              value={pronouns}
+              onChange={handleOnChange}
+              defaultValue={'default'}
             >
-              selecione
-            </option>
-            <option value="sr">senhor</option>
-            <option value="sra">senhora</option>
-            <option value="nf">sem formalidade</option>
-          </select>
-          <label htmlFor="pronouns">pronomes</label>
-          <select
-            name="pronouns"
-            value={pronouns}
-            onChange={handleOnChange}
-            defaultValue={'default'}
-          >
-            <option
-              value="default"
-              disabled
-            >
-              selecione
-            </option>
-            <option value="ele">ele/dele</option>
-            <option value="ele">ela/dela</option>
-            <option value="sn">prefere nao declarar</option>
-          </select>
-          <label htmlFor="name">Nome</label>
-          <input
-            onChange={handleOnChange}
-            value={name}
-            type="text"
-            name="name"
-          />
+              <option
+                value="default"
+                disabled
+              >
+                selecione
+              </option>
+              <option value="ele">ele/dele</option>
+              <option value="ele">ela/dela</option>
+              <option value="sn">prefere nao declarar</option>
+            </select>
+          </div>
         </div>
-        <label htmlFor="email">Email</label>
-        <input
-          value={email}
-          onChange={handleOnChange}
-          type="text"
-          name="email"
-        />
-        <label htmlFor="cpf">CPF</label>
-        <input
-          value={cpf}
-          onChange={handleOnChange}
-          type="text"
-          name="CPF"
-        />
-        <p>prosseguir para proxima página?</p>
+        <div className='info'>
+          <div className="line">
+            <label htmlFor="cpf">CPF</label>
+            <input
+              value={cpf}
+              onChange={handleOnChange}
+              type="text"
+              name="CPF"
+            />
+          </div>
+          <div className="line">
+            <label htmlFor="email">Email</label>
+            <input
+              value={email}
+              onChange={handleOnChange}
+              type="text"
+              name="email"
+            />
+          </div>
+        </div>
+        <div className='info'>
+          <div className="line">
+            <label htmlFor="name">Nome</label>
+            <input
+              onChange={handleOnChange}
+              value={name}
+              type="text"
+              name="name"
+            />
+          </div>
+        </div>
         <button
           type="button"
           className="btn">
